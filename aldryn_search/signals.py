7e3b99061a826242a7ca9a12584338f3cb1f390d
Ben Stähli <bnzk@bnzk.ch>
2023-09-05 15:26:32 +0200

    some django 4 compat

--- aldryn_search/signals.py
+++ aldryn_search/signals.py
@@ -7,5 +7,7 @@ Created on Nov 30, 2015
 from django.dispatch.dispatcher import Signal
 
 
-add_to_index = Signal(providing_args=['instance', 'object_action'])
-remove_from_index = Signal(providing_args=['instance', 'object_action'])
+# django 4 compat! add_to_index = Signal(providing_args=['instance', 'object_action'])
+add_to_index = Signal()
+# remove_from_index = Signal(providing_args=['instance', 'object_action'])
+remove_from_index = Signal()
